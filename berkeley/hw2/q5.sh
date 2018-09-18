061741797a80a888e84cfe319723f9bdbe26c44c
Jonathan Perl <perl.jonathan@gmail.com>
2018-09-18 10:49:11 -0400

    update q5 params / plot

--- berkeley/hw2/q5.sh
+++ berkeley/hw2/q5.sh
@@ -1,1 +1,1 @@
-python train_pg_f18.py InvertedPendulum-v2 -ep 1000 --discount 0.9 -n 100 -e 3 -l 2 -s 64 -b 2500 -lr 5e-3 -rtg --exp_name hc_b2500_r5e-3
+python train_pg_f18.py InvertedPendulum-v2 -ep 1000 --discount 0.9 -n 100 -e 3 -l 2 -s 64 -b 2500 -lr 1e-2 -rtg --exp_name hc_b2500_r1e-2
